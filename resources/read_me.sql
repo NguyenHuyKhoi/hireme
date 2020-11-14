62b315cf7694ccfe614ff308b7a9683dffaad6eb
Nguyễn_Huy_Khôi <khoi.nh170086@sis.hust.edu.vn>
2020-11-14 19:33:58 +0700

    Demo DB

---
+++ resources/read_me.sql
@@ -0,0 +1,5 @@
+Link thiết kế giao diện (có mô tả các API cần cho mỗi màn trong phần comment )  :
+	https://xd.adobe.com/view/8d71827b-cc59-450d-9bff-82001b073e30-d822/
+Trong file crowdsource_project.mdj có chứa 1 bản thiết kế DB để đáp ứng được giao diện trên, mn đọc rồi góp ý với, 
+danh sách API cho tất cả các màn em sẽ thêm vào ngày mai, link ko thay đổi ,vì đang tạo API thì thấy phải thiết kế CSDL trước thật 
+thì tạo API được.
